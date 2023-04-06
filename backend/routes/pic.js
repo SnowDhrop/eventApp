3c36c7cd59be41d22e88845de3095b5b890e637c
Snow Drop <nath.arditti@yahoo.fr>
2023-04-06 14:32:47 +0200

    finalization multer

--- backend/routes/pic.js
+++ backend/routes/pic.js
@@ -5,9 +5,10 @@ const router = express.Router();
 import * as pic from "./../controllers/pic.js";
 
 import auth from "./../middlewares/auth.js";
+import checkAccountValidity from "../middlewares/checkAccountValidity.js";
 
-router.post("/upload", auth, pic.uploadProf);
+router.post("/upload", auth, checkAccountValidity, pic.uploadProf);
 
-router.get("/prof", auth, pic.getProf);
+router.get("/prof", auth, checkAccountValidity, pic.getProf);
 
 export default router;
